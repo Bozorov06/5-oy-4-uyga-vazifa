f1bb022cc741bcb955a8e93b177cdf8780c8c09d
Bozorov06 <qoyilbekbozorov160@gmail.com>
2025-01-09 11:51:58 +0500

    ad

--- main.js
+++ main.js
@@ -27,16 +27,16 @@ buttons.forEach((button,index)=>{
 // // Ichidagi barcha bolalari (childNodes) chiqarilsin (matn yoki alert orqali).
 // // Ishlatiladigan mavzular: parentNode, siblings, childNodes.
 
-const items = document.querySelectorAll('#list li')
+// const items = document.querySelectorAll('#list li')
 
-items && items.forEach(item=>{
-   item && item.addEventListener('click', function(){
-    console.log("parent", this.parentNode);
-    console.log("prev sibling", this.previousElementSibling);
-    console.log("next sibling", this.nextElementSibling);
-   })
-   console.log("child", this.childNodes);
-})
+// items && items.forEach(item=>{
+//    item && item.addEventListener('click', function(){
+//     console.log("parent", this.parentNode);
+//     console.log("prev sibling", this.previousElementSibling);
+//     console.log("next sibling", this.nextElementSibling);
+//    })
+//    console.log("child", this.childNodes);
+// })
 
 
 // // ================================================================================================================================
@@ -44,28 +44,28 @@ items && items.forEach(item=>{
 // // Vazifa: Sahifada ikkita kvadrat (div) bo‘lsin. Har bir kvadrat ichida matn yozilgan bo‘lsin. Foydalanuvchi tugma bosganda, ushbu kvadratlarning o‘z ichidagi matnlari almashib qolsin.
 // // Ishlatiladigan mavzular: parentNode, appendChild, before, after.
 
-const card1 = document.getElementById('card1')
-const card2 = document.getElementById("card2");
-const btn = document.getElementById('btn')
-btn && btn.addEventListener('click', function(){
-    let temp;
-    temp=card1.innerHTML
-    card1.innerHTML = card2.innerHTML
-     card2.innerHTML=temp
-})
+// const card1 = document.getElementById('card1')
+// const card2 = document.getElementById("card2");
+// const btn = document.getElementById('btn')
+// btn && btn.addEventListener('click', function(){
+//     let temp;
+//     temp=card1.innerHTML
+//     card1.innerHTML = card2.innerHTML
+//      card2.innerHTML=temp
+// })
 // // ================================================================================================================================
 // // 4. "Qizil tugmalar" o‘yini
 // // Vazifa: Sahifada bir nechta tugmalar bo‘lsin. Har safar foydalanuvchi sahifaga kirganda, tugmalardan biriga avtomatik ravishda "qizil" klassi qo‘shiladi. Foydalanuvchi boshqa tugmalarni bosganda, yangi tugmaga "qizil" klassi o‘tadi, avvalgisi esa "qizil" klassini yo‘qotadi.
 // // Ishlatiladigan mavzular: classList.add, classList.remove, classList.contains.
 
-const buttons = document.querySelectorAll(".button")
-        buttons[0].classList.add("qizil");
- buttons.forEach((button) => {
-   button.addEventListener("click", function () {
-     buttons.forEach((value) => value.classList.remove("qizil"));
-     this.classList.add("qizil");
-   });
- });
+// const buttons = document.querySelectorAll(".button")
+//         buttons[0].classList.add("qizil");
+//  buttons.forEach((button) => {
+//    button.addEventListener("click", function () {
+//      buttons.forEach((value) => value.classList.remove("qizil"));
+//      this.classList.add("qizil");
+//    });
+//  });
 
 
 
@@ -76,16 +76,16 @@ const buttons = document.querySelectorAll(".button")
 // // Vazifa: Sahifada bir nechta paragraflar va bitta "Yangi paragraf qo‘shish" tugmasi bo‘lsin. Tugma bosilganda yangi paragraf qo‘shilsin va u maqolaning oxiriga yoki boshiga joylashtirilsin (foydalanuvchi oldindan tanlaydi).
 // // Ishlatiladigan mavzular: createElement, append, prepend.
 
-const card = document.getElementById('card')
-const btn = document.getElementById("btn");
-btn && btn.addEventListener('click',function(){
-    this.parentElement.innerHTML = 'qandaydir matn'
-    if (this.checkVisibility.checked) {
-        card.prepend(p)
-    }else{
-        card.append(p)
-    }
-})
+// const card = document.getElementById('card')
+// const btn = document.getElementById("btn");
+// btn && btn.addEventListener('click',function(){
+//     this.parentElement.innerHTML = 'qandaydir matn'
+//     if (this.checkVisibility.checked) {
+//         card.prepend(p)
+//     }else{
+//         card.append(p)
+//     }
+// })
 
 
 
@@ -95,42 +95,42 @@ btn && btn.addEventListener('click',function(){
 // // Ishlatiladigan mavzular: createElement, before, after, classList.add.
 
 
-const card = document.getElementById('card')
-const buttons = document.querySelectorAll('button')
+// const card = document.getElementById('card')
+// const buttons = document.querySelectorAll('button')
 
-buttons.length>0 && buttons.forEach(button=>{
-    button&&button.addEventListener('click',function(){
-        let block = document.createElement('div')
-        block.style.width = '200px'
-        block.style.height = "200px";
-        block.style.marginLeft='20px'
-        block.style.display = "inline-block";
-        block.style.backgroundColor = "blue";
-        if (this.innerHTML == 'left') {
-            card.before(block)
-        }else{
-            card.after(block);
-        }
-    })
-})
+// buttons.length>0 && buttons.forEach(button=>{
+//     button&&button.addEventListener('click',function(){
+//         let block = document.createElement('div')
+//         block.style.width = '200px'
+//         block.style.height = "200px";
+//         block.style.marginLeft='20px'
+//         block.style.display = "inline-block";
+//         block.style.backgroundColor = "blue";
+//         if (this.innerHTML == 'left') {
+//             card.before(block)
+//         }else{
+//             card.after(block);
+//         }
+//     })
+// })
 
 
 // // ================================================================================================================================
 
 
-const hideButtons = document.querySelectorAll('.hide')
-const show = document.querySelector('#show')
-const mainButtons = document.querySelectorAll('.main')
+// const hideButtons = document.querySelectorAll('.hide')
+// const show = document.querySelector('#show')
+// const mainButtons = document.querySelectorAll('.main')
 
-hideButtons.length && hideButtons.forEach(hide=>{
-   hide && hide.addEventListener('click', function(){
-    this.previousElementSibling.style.display = 'none'
-   })
-})
+// hideButtons.length && hideButtons.forEach(hide=>{
+//    hide && hide.addEventListener('click', function(){
+//     this.previousElementSibling.style.display = 'none'
+//    })
+// })
 
-show && show.addEventListener('click', function(){
-    mainButtons.style.display= 'inline'
-})
+// show && show.addEventListener('click', function(){
+//     mainButtons.style.display= 'inline'
+// })
 
 
 // // ================================================================================================================================
@@ -138,21 +138,21 @@ show && show.addEventListener('click', function(){
 // // Vazifa: Sahifada bir nechta matnli elementlar (p) va "Kesish" tugmasi bo‘lsin. Foydalanuvchi biror matnni tanlab, uning yonidagi "Kesish" tugmasini bossa, o‘sha matn kesilib, boshqa bir bo‘sh joyga (kvadrat) o‘tkazilsin.
 // // Ishlatiladigan mavzular: removeChild, appendChild.
 
-  const buttons = document.querySelectorAll(".btn");
+  // const buttons = document.querySelectorAll(".btn");
 
-  buttons.forEach((btn) => {
-    btn && btn.addEventListener("click", function () {
-      const p = this.parentElement;
-      const Text = window.getSelection().toString(); 
+  // buttons.forEach((btn) => {
+  //   btn && btn.addEventListener("click", function () {
+  //     const p = this.parentElement;
+  //     const Text = window.getSelection().toString(); 
 
-      if (selectedText) {
-        const yangiP = document.createElement("p");
-        yangiP.textContent = Text;
-        document.getElementsByClassName("square").appendChild(yangiP);
-        p.textContent = p.textContent.replace(Text, "");
-      }
-    });
-  });
+  //     if (selectedText) {
+  //       const yangiP = document.createElement("p");
+  //       yangiP.textContent = Text;
+  //       document.getElementsByClassName("square").appendChild(yangiP);
+  //       p.textContent = p.textContent.replace(Text, "");
+  //     }
+  //   });
+  // });
 
 
 
@@ -189,23 +189,23 @@ show && show.addEventListener('click', function(){
 // // Ishlatiladigan mavzular: style, classList.toggle, classList.add.
 
 
- const hides = document.querySelectorAll(".hide");
- const blocks = document.querySelectorAll(".block"); 
- const show = document.getElementById("show");
+//  const hides = document.querySelectorAll(".hide");
+//  const blocks = document.querySelectorAll(".block"); 
+//  const show = document.getElementById("show");
 
  
- hides.forEach((button, index) => {
-   button.addEventListener("click", () => {
-     blocks[index].style.display = "none"; 
-   });
- });
+//  hides.forEach((button, index) => {
+//    button.addEventListener("click", () => {
+//      blocks[index].style.display = "none"; 
+//    });
+//  });
 
 
- show.addEventListener("click", () => {
-   blocks.forEach((block) => {
-     block.style.display = "block"; 
-   });
- });
+//  show.addEventListener("click", () => {
+//    blocks.forEach((block) => {
+//      block.style.display = "block"; 
+//    });
+//  });
 
 
 
@@ -216,26 +216,26 @@ show && show.addEventListener('click', function(){
 // // Vazifa: Sahifada bir nechta rangli kvadratlar (div) bo‘lsin. Har bir kvadratni boshqa kvadrat bilan almashtirish tugmasi bo‘lsin. Tugma bosilganda, o‘sha kvadrat boshqa tanlangan kvadrat bilan joylarini almashtirsin (ularni DOM ichida joylashtirish tartibi o‘zgaradi).
 // // Ishlatiladigan mavzular: parentNode, replaceChild, appendChild.
 
-const container = document.getElementById("container");
-const swapButtons = document.querySelectorAll(".swapButton");
+// const container = document.getElementById("container");
+// const swapButtons = document.querySelectorAll(".swapButton");
 
-swapButtons.forEach((button) => {
-  button.addEventListener("click", function () {
-    const square1 = this.parentNode;
-    let square2 = square1.nextElementSibling;
-    if (!square2) {
-      square2 = square1.previousElementSibling;
-    }
+// swapButtons.forEach((button) => {
+//   button.addEventListener("click", function () {
+//     const square1 = this.parentNode;
+//     let square2 = square1.nextElementSibling;
+//     if (!square2) {
+//       square2 = square1.previousElementSibling;
+//     }
 
-    swapSquares(square1, square2);
-  });
-});
+//     swapSquares(square1, square2);
+//   });
+// });
 
-function swapSquares(square1, square2) {
-  const parent = square1.parentNode;
-  parent.replaceChild(square2, square1);
-  parent.appendChild(square1);
-}
+// function swapSquares(square1, square2) {
+//   const parent = square1.parentNode;
+//   parent.replaceChild(square2, square1);
+//   parent.appendChild(square1);
+// }
 
 // // ================================================================================================================================
 // // 13. Matn uzunligini ko‘rsatish
@@ -248,17 +248,17 @@ function swapSquares(square1, square2) {
 // // Vazifa: Sahifada bir nechta div elementlar bo‘lsin, ular ustida "Ball qo‘shish" tugmasi bo‘lsin. Har safar tugma bosilganda, blok ichidagi matn sifatida 1 ball qo‘shiladi (0 dan boshlanadi). Foydalanuvchi sahifada qancha ball to‘plaganini hisoblash uchun pastda umumiy ball ko‘rsatadigan alohida blok bo‘lsin.
 // // Ishlatiladigan mavzular: textContent, classList.add, classList.contains.
 
-let totalPoints = 0;
-document.querySelectorAll('.addscore').forEach(button =>{
-  button.addEventListener('click', function(){
-    const score = this.previousElementSibling
-    let currentScore = parseInt(score.textContent);
-    currentScore++
-    score.textContent = currentScore
-    totalPoints++
-    document.getElementById('totalPoint').textContent = totalPoints
-  })
-})
+// let totalPoints = 0;
+// document.querySelectorAll('.addscore').forEach(button =>{
+//   button.addEventListener('click', function(){
+//     const score = this.previousElementSibling
+//     let currentScore = parseInt(score.textContent);
+//     currentScore++
+//     score.textContent = currentScore
+//     totalPoints++
+//     document.getElementById('totalPoint').textContent = totalPoints
+//   })
+// })
 
 
 
@@ -270,16 +270,16 @@ document.querySelectorAll('.addscore').forEach(button =>{
 // Ishlatiladigan mavzular: parentNode, before, after.
 
 
-document.getElementById("itemsContainer").addEventListener("click", function (event){
-    const button = event.target;
-    if (button.tagName === "BUTTON") {
-      const item = button.closest(".item");
-      const parent = item.parentNode;
-      if (button.classList.contains("moveUp") && item.previousElementSibling) {
-        parent.insertBefore(item, item.previousElementSibling);
-      }
-      if (button.classList.contains("moveDown") && item.nextElementSibling) {
-        parent.insertBefore(item, item.nextElementSibling.nextElementSibling);
-      }
-    }
-  });
+// document.getElementById("itemsContainer").addEventListener("click", function (event){
+//     const button = event.target;
+//     if (button.tagName === "BUTTON") {
+//       const item = button.closest(".item");
+//       const parent = item.parentNode;
+//       if (button.classList.contains("moveUp") && item.previousElementSibling) {
+//         parent.insertBefore(item, item.previousElementSibling);
+//       }
+//       if (button.classList.contains("moveDown") && item.nextElementSibling) {
+//         parent.insertBefore(item, item.nextElementSibling.nextElementSibling);
+//       }
+//     }
+//   });
